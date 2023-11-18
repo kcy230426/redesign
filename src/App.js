cec48c5a3a8ac0a320c9e31f6f915b8e6840ca02
kcy230426 <purple_galaxy@naver.com>
2023-11-18 13:19:15 +0900

    해쉬라우터로 변경

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {HashRouter, Route, Routes} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 import Header from './component/Header'
 import Contents from './component/Contents'
@@ -12,14 +12,14 @@ import cb from './db/mapping.json'
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
     <Header dbnav={db.nav} dbnr={db.nr} dbm={db.men} dbwm={db.women} dbk={db.kids} dbs={db.sale} ms={db.menus}></Header>
     <Routes>
       <Route path="/" element={<div><Contents dbd1={db.d1} dbd2={db.d2} dbd3={db.d3} /></div>}></Route>
       <Route path="/nwf" element={<Nwf data={cb.db}/>} />
     </Routes>
     <Footer dbf={db.footer} dbq={cb.db} dbs={db.fsns} dbm={db.fmenus} dbi={db.finfo} dbn={db.fnotice}/>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
